257b8577944a773b611d25649e3d19e88ac19a12
Patka879 <mysiak.patrycja@gmail.com>
2023-03-03 00:19:02 +0100

    features folder, cartSlice and Store creates

---
+++ starter/src/features/cartSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    cartItems : 0,
+    amount: 0,
+    total: 0,
+    isLoading: true
+}
+
+const cartSlice = createSlice({
+    name: cart,
+    initialState
+})
